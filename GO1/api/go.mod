dca565eb3b858c5db33be5a2a19b8450248e8014
inspirasiprogrammer <bDa3qviHDaWS6aL>
2020-10-13 12:40:51 +0700

    Update  Rest Api

--- GO1/api/go.mod
+++ GO1/api/go.mod
@@ -2,4 +2,7 @@ module github.com/TutorialEdge/create-rest-api-in-go-tutorial
 
 go 1.12
 
-require github.com/gorilla/mux v1.7.2
+require (
+	github.com/gorilla/mux v1.7.2
+	github.com/jinzhu/gorm v1.9.16
+)
---
+++ GO1/api/latihan.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var db *gorm.DB
+var err error
+
+type Booking struct {
+	Id      int    `json:"id"`
+	User    string `json:"user"`
+	Members int    `json:"members"`
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to HomePage!")
+	fmt.Println("Endpoint Hit: HomePage")
+}
+
+func handleRequests() {
+	log.Println("Starting development server at http://127.0.0.1:10000/")
+	log.Println("Quit the server with CONTROL-C.")
+	// creates a new instance of a mux router
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+
+func main() {
+	// Please define your user name and password for my sql.
+	db, err = gorm.Open("mysql", "root:AKSHATGUPTa@tcp(127.0.0.1:3306)/Football?charset=utf8&parseTime=True")
+	// NOTE: See we’re using = to assign the global var
+	// instead of := which would assign it only in this function
+
+	if err != nil {
+		log.Println("Connection Failed to Open")
+	} else {
+		log.Println("Connection Established")
+	}
+
+	db.AutoMigrate(&Booking{})
+	handleRequests()
+}
